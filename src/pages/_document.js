52ad139d00aa65eb72a0ba50b682746a3dde0644
Rahul Pradhan <74361662+rahulp18@users.noreply.github.com>
2022-08-14 00:23:52 -0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,40 +1,28 @@
-import { ServerStyleSheets } from '@material-ui/core/styles';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-  ctx.renderPage = () => {
-    return originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
-  };
-  const initialProps = await Document.getInitialProps(ctx);
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
+const Document = () => {
+  return (
+    <Html lang='en'>
+      <Head>
+        {/* Using 'Inter' as default font-family*/}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymus'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 };
+
+export default Document;
